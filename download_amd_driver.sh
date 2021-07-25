99363f3602290c1dc147910e954f385addf3a91e
Simon <simonschwaiger1308@gmail.com>
2021-07-25 11:32:39 +0200

    Add AMD driver download

---
+++ download_amd_driver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# automatically downloads amdgpu pro driver version 21.02 for ubuntu 20.04
+wget --referer=http://support.amd.com  https://drivers.amd.com/drivers/linux/amdgpu-pro-21.20-1271047-ubuntu-20.04.tar.xz
